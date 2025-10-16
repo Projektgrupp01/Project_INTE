60854bfb7e7f57c6e9b56f5d34804b253be66c78
Alva Backhans <alva.backhans@gmail.com>
2025-10-16 11:52:59 +0200

    playerspelltest

--- src/test/java/io/github/Projektgrupp01/Project_INTE/PlayerSpellTest.java
+++ src/test/java/io/github/Projektgrupp01/Project_INTE/PlayerSpellTest.java
@@ -3,22 +3,45 @@ import io.github.Projektgrupp01.Project_INTE.spells.Fire;
 import io.github.Projektgrupp01.Project_INTE.spells.Spell;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Collections;
+
 import org.junit.jupiter.api.*;
 
 import io.github.Projektgrupp01.Project_INTE.creatures.BasePlayer;
-import io.github.Projektgrupp01.Project_INTE.creatures.decorators.TripleHealthDecorator;
 
 public class PlayerSpellTest {
-    private BasePlayer player;
+    Spell fire = new Fire();
+    @BeforeEach
     @Test
 	void playerCanLearnSpell() {
-		Spell fire = new Fire();
+        BasePlayer player = new BasePlayer();
 		player.learnSpell(fire);
 		assertTrue(player.getSpellBook().contains(fire));
 		assertEquals(1, player.getSpellBook().size());
 	}
     @Test
-    void 
+    void negativeEnergyCostException() {
+        BasePlayer player = new BasePlayer();
+        player.learnSpell(fire);
+        assertThrows(IllegalArgumentException.class,() -> { 
+            player.learnSpell(fire);;});
+    }
+    @Test
+    void canRemoveSpell(){
+        BasePlayer player = new BasePlayer();
+        player.learnSpell(fire);
+        player.forgetSpell(fire);
+        assertEquals(Collections.emptySet(), player.getSpellBook());
+    }
+    @Test
+    void removingSpellThatDoesNotExistException(){
+        BasePlayer player = new BasePlayer();
+        assertThrows(IllegalArgumentException.class, () -> {
+            player.forgetSpell(fire);
+        });
+    }
+
     
     
     
