f78236aacc190a3531a27314971a52eb69b8f6c2
Alva Backhans <alva.backhans@gmail.com>
2025-10-18 19:28:44 +0200

    added tests in spelltests

--- src/test/java/io/github/Projektgrupp01/Project_INTE/PlayerSpellTest.java
+++ src/test/java/io/github/Projektgrupp01/Project_INTE/PlayerSpellTest.java
@@ -10,37 +10,61 @@ import org.junit.jupiter.api.*;
 
 import io.github.Projektgrupp01.Project_INTE.creatures.BasePlayer;
 
-public class PlayerSpellTest {
-    Spell fire = new Fire();
-    @BeforeEach
+class PlayerSpellTest {
+
+    private BasePlayer player;
+    private Spell fire;
+	@BeforeEach
+	void setUp() {
+		player = new BasePlayer();
+        fire = new Fire();
+	}
     @Test
+    //learnSpell tests
 	void playerCanLearnSpell() {
-        BasePlayer player = new BasePlayer();
 		player.learnSpell(fire);
 		assertTrue(player.getSpellBook().contains(fire));
 		assertEquals(1, player.getSpellBook().size());
 	}
     @Test
-    void negativeEnergyCostException() {
-        BasePlayer player = new BasePlayer();
+    void learnSameSpellTwiceException() {
         player.learnSpell(fire);
         assertThrows(IllegalArgumentException.class,() -> { 
-            player.learnSpell(fire);;});
+            player.learnSpell(fire);});
     }
     @Test
+    void learnNullSpellException(){
+        assertThrows(NullPointerException.class, () -> {
+            player.learnSpell(null);
+        });
+    }
+    //getSpellbook Tests
+    @Test
+    void getEmptySpellbookTest(){
+        assertEquals(Collections.emptySet(), player.getSpellBook());
+    }
+    //forgetSpell tests
+    @Test
     void canRemoveSpell(){
-        BasePlayer player = new BasePlayer();
         player.learnSpell(fire);
         player.forgetSpell(fire);
         assertEquals(Collections.emptySet(), player.getSpellBook());
     }
     @Test
+    void forgetNullSpellException(){
+        player.learnSpell(fire);
+        assertThrows(NullPointerException.class, ()->{
+            player.forgetSpell(null);
+        });
+    }
+    @Test
     void removingSpellThatDoesNotExistException(){
-        BasePlayer player = new BasePlayer();
         assertThrows(IllegalArgumentException.class, () -> {
             player.forgetSpell(fire);
         });
     }
+
+
 
     
     
