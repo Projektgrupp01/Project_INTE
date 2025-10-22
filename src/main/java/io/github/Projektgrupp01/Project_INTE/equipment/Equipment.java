55aec51d49da2b1daaaa39397f7103ba07eacff4
Filip SW <filipsw1@gmail.com>
2025-10-22 20:29:14 +0200

    Created basic Equipment class

---
+++ src/main/java/io/github/Projektgrupp01/Project_INTE/equipment/Equipment.java
@@ -0,0 +1,18 @@
+package io.github.Projektgrupp01.Project_INTE.equipment;
+
+public class Equipment {
+
+    private String name;
+    private EquipmentType type;
+
+    public Equipment(String name, EquipmentType type) {
+
+        this.name = name;
+        this.type = type;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+}
