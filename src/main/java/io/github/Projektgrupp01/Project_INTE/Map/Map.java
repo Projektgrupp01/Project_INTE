2860ed118a29828a7b62865611f591cc7e61a4fb
EvFo <e.forsen@proton.me>
2025-10-25 20:27:14 +0200

    uppdaterat getMapTest + Maps toString-metod

--- src/main/java/io/github/Projektgrupp01/Project_INTE/Map/Map.java
+++ src/main/java/io/github/Projektgrupp01/Project_INTE/Map/Map.java
@@ -1,40 +1,11 @@
 package io.github.Projektgrupp01.Project_INTE.Map;
 
-import java.util.Arrays;
+public interface Map {
 
-public class Map {
+    char[][] getMap();
 
-    private int x;
-    private int y;
-    private char [][] mapArray;
+    void createMap();
 
-    public Map(){
-        this(20,20);
-    }
-
-    public Map(int x, int y){
-        this.x = x;
-        this.y = y;
-        createArray(x, y);
-    }
-
-    public char[][] getMap(){
-        return mapArray;
-    }
-
-    private void createArray(int x, int y){
-        mapArray = new char[x][y];
-        //flytta till createmap-metod sen
-        for(char[] row: mapArray){
-            Arrays.fill(row, '#');
-        }
-    }
-
-    @Override
-    public String toString(){
-        return Arrays.deepToString(mapArray);
-    }
-
-
+    String toString();
 
 }
--- src/test/java/io/github/Projektgrupp01/Project_INTE/MapTest.java
+++ src/test/java/io/github/Projektgrupp01/Project_INTE/MapTest.java
@@ -5,29 +5,60 @@ import org.junit.jupiter.api.Test;
 import io.github.Projektgrupp01.Project_INTE.Map.Map;
 
 import java.util.Arrays;
+import java.lang.StringBuilder;
 
+//borde verkligen döpa om testen
+// & byt namn på Map/klassen så det blir tydligt att det endast är för
+// mapGenerator
+//o inte interfacet
+//testa interface?
 public class MapTest {
 
     @Test
-    void getMapTest(){ //ändra t ngn annan assert? lista?
+    void getMapTest_notNull(){ //ändra t ngn annan assert? lista?
         /*Map map = new Map();
         assertEquals(getMap(), new Map(3,3));*/
         Map map = new Map(3, 4);
-        assertNotNull(map.getMap());
+        assertNotNull(map.getMap()); //borde jag ha assertequals eller nått här?
+        //ngt annat test som bekräftar att map:ens utseende är korrekt
+    }
+
+    @Test //gör test som bekräftar att innehållet/utseendet är korrekt
+    void getMapTest(){
+        Map map = new Map();
+
     }
 
     @Test
-    void toStringTest_NoInitializers(){
+    void createMapTest(){
+
+    }
+
+    @Test
+    void generateMapOnPlayerWalkTest(){
+
+    }
+
+    @Test
+    void rememberMapTest(){ //byt namn t ngt mer begripligt
+
+    }
+
+    @Test //borde man inte kunna göra detta utan stringen...?
+    void Map_toStringTest_NoInitializers(){
         Map map = new Map();
-        char[][] array = new char[20][20];
+        /*char[][] array = new char[20][20];
         for(char[] row: array) {
             Arrays.fill(row, '#');
-        }
-        assertEquals(map.toString(), Arrays.deepToString(array));
+        }*/
+        StringBuilder str = new StringBuilder();
+        //initialisera sträng genom loop som tar map-arrayens antal rader som längdarg,
+        //lägg in char '#' på alla positioner, bryt med \n efter varje loop
+        assertEquals(map.toString(), /*Arrays.deepToString(array)*/);
     }
 
     @Test
-    void toStringTest_WithInitializers(){
+    void Map_toStringTest_WithInitializers(){
         Map map = new Map(3,11);
         char[][] array = new char[3][11];
         for(char[] row: array) {
