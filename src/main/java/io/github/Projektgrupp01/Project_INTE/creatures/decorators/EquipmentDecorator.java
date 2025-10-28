9fbd45a4493a417a645f4c76354c69fdd3c2c3f0
Filip SW <filipsw1@gmail.com>
2025-10-28 21:07:27 +0100

    Added EquipmentDecorator tests for bonuses and validation
    Health/energy bonus tests (affects max, not current)
    Decorator stacking test
    Level requirement validation tests

--- src/main/java/io/github/Projektgrupp01/Project_INTE/creatures/decorators/EquipmentDecorator.java
+++ src/main/java/io/github/Projektgrupp01/Project_INTE/creatures/decorators/EquipmentDecorator.java
@@ -2,6 +2,7 @@ package io.github.Projektgrupp01.Project_INTE.creatures.decorators;
 
 import io.github.Projektgrupp01.Project_INTE.equipment.Equipment;
 import io.github.Projektgrupp01.Project_INTE.creatures.Player;
+import io.github.Projektgrupp01.Project_INTE.creatures.BasePlayer;
 
 public class EquipmentDecorator extends PlayerDecorator {
 
@@ -9,10 +10,42 @@ public class EquipmentDecorator extends PlayerDecorator {
 
     public EquipmentDecorator(Player player, Equipment equipment) {
         super(player);
+
+        if (equipment == null) {
+            throw new IllegalArgumentException("Equipment cannot be null");
+        }
+
+        if (equipment.getLevelRequirement() > player.getLevel()) {
+            throw new IllegalArgumentException("Player too low level to equip " + equipment.getName() + ", " +
+                    "Required level: " + equipment.getLevelRequirement() + ", " +
+                    "Current level: " + player.getLevel());
+        }
+
         this.equipment = equipment;
     }
 
     public Equipment getEquipment() {
         return equipment;
     }
+
+    public int getMaxHealth() {
+        int baseMaxHealth = super.getMaxHealth();
+
+        if (equipment.getHealthBonus() != null) {
+            return baseMaxHealth + equipment.getHealthBonus();
+        }
+
+        return baseMaxHealth;
+    }
+
+    public int getMaxEnergy() {
+        int baseMaxEnergy = super.getMaxEnergy();
+
+        if (equipment.getEnergyBonus() != null) {
+            return baseMaxEnergy + equipment.getEnergyBonus();
+        }
+
+        return baseMaxEnergy;
+    }
+
 }
