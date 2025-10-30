40ee92aca6354d4685ede813987e7054049e8996
Filip SW <filipsw1@gmail.com>
2025-10-30 21:34:38 +0100

    Added Inventory class and test class (TDD)
    Default capacity = 30 items
    Add/remove with validation
    Cannot add null or when inventory is full

---
+++ src/main/java/io/github/Projektgrupp01/Project_INTE/equipment/Inventory.java
@@ -0,0 +1,72 @@
+package io.github.Projektgrupp01.Project_INTE.equipment;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Inventory {
+
+    private static final int DEFAULT_MAX_CAPACITY = 30;
+    private int maxCapacity;
+    private List<Equipment> inventoryItems;
+
+    public Inventory() {
+        this(DEFAULT_MAX_CAPACITY);
+    }
+
+    public Inventory(int maxCapacity) {
+        if (maxCapacity <= 0) {
+            throw new IllegalArgumentException("Max capacity must be bigger than 0");
+        }
+
+        this.maxCapacity = maxCapacity;
+        this.inventoryItems = new ArrayList<>();
+    }
+
+    public int getSize() {
+        return inventoryItems.size();
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void addItem(Equipment equipment) {
+        if (equipment == null) {
+            throw new IllegalArgumentException("Cannot add null object");
+        }
+
+        if (isFull()) {
+            throw new IllegalStateException("Inventory is full");
+        }
+        inventoryItems.add(equipment);
+
+    }
+
+    public void removeItem(Equipment equipment) {
+        if (!inventoryItems.contains(equipment)) {
+            throw new IllegalArgumentException("Item does not exist in inventory");
+        }
+        inventoryItems.remove(equipment);
+    }
+
+    public boolean isFull() {
+        return inventoryItems.size() >= maxCapacity;
+    }
+
+    public boolean contains(Equipment equipment) {
+        return inventoryItems.contains(equipment);
+    }
+
+    public void clear() {
+        inventoryItems.clear();
+    }
+
+    public int getRemainingInventorySpace() {
+        return maxCapacity - inventoryItems.size();
+    }
+
+    public List<Equipment> getItems() {
+        return Collections.unmodifiableList(inventoryItems);
+    }
+}
---
+++ src/test/java/io/github/Projektgrupp01/Project_INTE/equipment/InventoryTest.java
@@ -0,0 +1,146 @@
+package io.github.Projektgrupp01.Project_INTE.equipment;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.List;
+
+public class InventoryTest {
+
+    @Test
+    void newInventoryIsEmpty() {
+        Inventory Inventory = new Inventory();
+        assertEquals(0, Inventory.getSize());
+    }
+
+    @Test
+    void inventoryHasAMaxCapacityOfThirty() {
+
+        Inventory inventory = new Inventory();
+        assertEquals(30, inventory.getMaxCapacity());
+    }
+
+    @Test
+    void canAddAnItemToInventory() {
+        Inventory inventory = new Inventory();
+        Equipment sword = new Equipment("Sword", EquipmentType.WEAPON);
+
+        inventory.addItem(sword);
+
+        assertEquals(1, inventory.getSize());
+    }
+
+    @Test
+    void cannotAddNullObjectToInventory() {
+        Inventory inventory = new Inventory();
+
+        try {
+            inventory.addItem(null);
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+
+        }
+    }
+
+    @Test
+    void cannotAddItemIfInventoryIsFull() {
+        Inventory inventory = new Inventory(2);
+
+        Equipment sword = new Equipment("Sword", EquipmentType.WEAPON);
+        Equipment shield = new Equipment("Shield", EquipmentType.SHIELD);
+        Equipment helmet = new Equipment("Helmet", EquipmentType.HELMET);
+
+        inventory.addItem(sword);
+        inventory.addItem(shield);
+
+        try {
+            inventory.addItem(helmet);
+            fail("ExpectedIllegalStateException");
+        } catch (IllegalStateException e) {
+        }
+    }
+
+    @Test
+    void canRemoveItemFromInventory() {
+        Inventory inventory = new Inventory();
+
+        Equipment sword = new Equipment("sword", EquipmentType.WEAPON);
+
+        inventory.addItem(sword);
+        inventory.removeItem(sword);
+
+        assertEquals(0, inventory.getSize());
+    }
+
+    @Test
+    void cannotRemoveItemNotInInventory() {
+        Inventory inventory = new Inventory();
+        Equipment sword = new Equipment("Sword", EquipmentType.WEAPON);
+        Equipment shield = new Equipment("Shield", EquipmentType.SHIELD);
+        inventory.addItem(sword);
+
+        try {
+            inventory.removeItem(shield);
+            fail("Cannot remove item thats not in inventory");
+        } catch (IllegalArgumentException e) {
+
+        }
+    }
+
+    @Test
+    void itemInInventoryReturnsTrue() {
+        Inventory inventory = new Inventory();
+        Equipment sword = new Equipment("Super sword", EquipmentType.WEAPON);
+
+        inventory.addItem(sword);
+
+        assertTrue(inventory.contains(sword));
+    }
+
+    @Test
+    void removeAllItems() {
+        Inventory inventory = new Inventory();
+        Equipment sword = new Equipment("Sword", EquipmentType.WEAPON);
+        Equipment shield = new Equipment("Shield", EquipmentType.SHIELD);
+        Equipment helmet = new Equipment("Helmet", EquipmentType.HELMET);
+
+        inventory.addItem(helmet);
+        inventory.addItem(shield);
+        inventory.addItem(sword);
+
+        inventory.clear();
+
+        assertEquals(0, inventory.getSize());
+    }
+
+    @Test
+    void getRemaindingInventorySpaceWorks() {
+        Inventory inventory = new Inventory();
+        Equipment sword = new Equipment("Sword", EquipmentType.WEAPON);
+
+        inventory.addItem(sword);
+        assertEquals(29, inventory.getRemainingInventorySpace());
+    }
+
+    @Test
+    void getItemsReturnsAllItemsInInventory() {
+        Inventory inventory = new Inventory();
+        Equipment sword = new Equipment("Sword", EquipmentType.WEAPON);
+        Equipment shield = new Equipment("Shield", EquipmentType.SHIELD);
+        Equipment helmet = new Equipment("Helmet", EquipmentType.HELMET);
+
+        inventory.addItem(helmet);
+        inventory.addItem(sword);
+        inventory.addItem(shield);
+
+        List<Equipment> inventoryItems = inventory.getItems();
+
+        assertEquals(3, inventoryItems.size());
+        assertTrue(inventoryItems.contains(helmet));
+        assertTrue(inventoryItems.contains(sword));
+        assertTrue(inventoryItems.contains(shield));
+    }
+
+}
