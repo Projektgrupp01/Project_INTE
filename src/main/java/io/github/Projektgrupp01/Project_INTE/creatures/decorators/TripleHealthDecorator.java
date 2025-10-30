103b59dbad505860dcc5866f285a91beed2620ec
Isac-Nordborg <isac.nordborg@gmail.com>
2025-10-30 23:15:45 +0100

    Fixes for coverage

--- src/main/java/io/github/Projektgrupp01/Project_INTE/creatures/decorators/TripleHealthDecorator.java
+++ src/main/java/io/github/Projektgrupp01/Project_INTE/creatures/decorators/TripleHealthDecorator.java
@@ -3,16 +3,11 @@ package io.github.Projektgrupp01.Project_INTE.creatures.decorators;
 import io.github.Projektgrupp01.Project_INTE.creatures.Player;
 
 public class TripleHealthDecorator extends PlayerDecorator{
-	private String name = "Triple health!";
 	public TripleHealthDecorator (Player p) {
 		super(p);
 	}
 	@Override
 	public int getHealth() {
 		return super.getHealth() * 3;
-	}
-	@Override
-	public String getName() {
-		return name;
 	}
 }
